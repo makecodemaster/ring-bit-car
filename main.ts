9e6cdc43b971eff9b2fb8263b9f81f7701be4351
makecodemaster <64723931+makecodemaster@users.noreply.github.com>
2020-06-18 18:32:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 90)
+})
+input.onButtonPressed(Button.AB, function () {
+    RingbitCar.running_distance(RingbitCar.Direction_run.forward, 5)
+})
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 90)
+})
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.forever(function () {
 	
 })
